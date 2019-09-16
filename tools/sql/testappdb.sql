2035cdc3e680d036addfc7785ecb2c1f31e6505b
Vjacheslav V. Borisov <slavb18@gmail.com>
2019-09-16 03:03:42 -0400

    1. fix charcter set 2. privileges for localhost

--- tools/sql/testappdb.sql
+++ tools/sql/testappdb.sql
@@ -1,2 +1,3 @@
 create database testapp;
 grant all privileges on testapp.* to 'testapp'@'%' identified by '1qazxsw2';
+grant all privileges on testapp.* to 'testapp'@'localhost' identified by '1qazxsw2';
